d1f983e6df4443afa5434f47c659dfa01dab8588
josekron <josekron@gmail.com>
2024-01-23 18:18:11 +0000

    Fix incompatibility between graphql and vitest

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
-    coverage: {
-      reporter: ["text", "json-summary", "json", "html"],
-      include: ["src/**"],
-      reportOnFailure: true,
+    deps: {
+      fallbackCJS: true,
     },
   },
 });
